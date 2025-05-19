143992e9b90865006b1be686eef515c310c03ccb
JPCodes44 <jp.mak44@gmail.com>
2025-05-19 12:06:43 +0200

    fixed the issue where you cant keep typing after typing smth into the input field

--- src/components/todoform.tsx
+++ src/components/todoform.tsx
@@ -1,7 +1,49 @@
 'use client';                             // ← Must be FIRST line
-import { DetailedHTMLProps, FormEvent, InputHTMLAttributes, JSX, ReactHTMLElement, useState } from 'react';
+import { ChangeEvent, DetailedHTMLProps, FormEvent, InputHTMLAttributes, JSX, useCallback, useState } from 'react';
 import { TodoList } from '../components/todolist'
 import { FC } from 'react'
+import React from 'react';
+
+
+type InputCardProps = {
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+type LabelProps = {
+    children?: React.ReactNode
+}
+
+type PaddedDivProps = {
+    children?: React.ReactNode
+}
+
+
+const InputCard: FC<InputCardProps> = ({value, onChange}) => {
+    return(
+    <input
+        type="string"
+        style={{display: "flex", justifyContent: "center", borderColor: "white", borderWidth: "1px", padding: "10px"}}
+        value={value}
+        onChange = {onChange}
+        placeholder="Type something">
+    </input>)
+} 
+
+
+const Label: FC<LabelProps> = ({children}) => {
+    return(
+        <label style={{display: "flex", justifyContent: "center"}}>{children}</label>)
+}
+
+
+const PaddedDiv: FC<PaddedDivProps> = React.memo(({children}) => {
+    return(
+        <div
+        style={{padding: "50px", display: "flex", justifyContent: "center"}}
+    >{children}
+    </div>)
+})
 
 export const TodoForm = () => {
 
@@ -23,37 +65,6 @@ export const TodoForm = () => {
         setValue(''); 
     }
 
-    type InputCardProps = {
-        value: string;
-        onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    }
-
-    const InputCard = ({value, onChange}: InputCardProps): JSX.Element => {
-        return(
-        <input
-            type="string"
-            style={{display: "flex", justifyContent: "center"}}
-            value={value}
-            onChange = {e => setValue(e.target.value)}
-            placeholder="Type something">
-        </input>)
-    } 
-
-    type LabelProps = {
-        children?: React.ReactNode
-    }
-
-    const Label = ({children}: LabelProps): JSX.Element => {
-        return(
-            <label style={{display: "flex", justifyContent: "center"}}>{children}</label>)
-    }
-
-    const PaddedDiv = ({children}: LabelProps): JSX.Element => {
-        return(
-            <div
-            style={{padding: "50px", display: "flex", justifyContent: "center"}}
-        >{children}</div>)
-    }
 
     const SubmitButton = (): JSX.Element => {
         return(<button type="submit" style={{ display: 'none' }}></button>)
@@ -62,11 +73,13 @@ export const TodoForm = () => {
     const DeleteButton = (): JSX.Element => {
         return(<button type="button" onClick={handleDelete}>Delete</button>)}
 
+    const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {setValue(e.target.value)}, [setValue])
+
     return (
         <form onSubmit={handleSubmit} style={{padding: "50px", display: "flex", flexDirection: "column", justifyContent: "center"}}>
             <PaddedDiv>
                 <Label>
-                    <InputCard value={value} onChange={e => setValue(e.target.value)}/>
+                <InputCard value={value} onChange={handleChange}/>
                 </Label>
             </PaddedDiv>
             <PaddedDiv>
